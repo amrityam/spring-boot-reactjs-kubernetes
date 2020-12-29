e1b82e897d74c13e362976ab6716aae55d8b47b2
Amrityam_Rout <amrityamlive@gmail.com>
2020-12-29 11:22:26 +0530

    Added spring boot skeleton for backend service

---
+++ corona-tracker-backend/src/main/java/com/hackforfun/coronatrackerbackend/CoronaTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackforfun.coronatrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ corona-tracker-backend/src/test/java/com/hackforfun/coronatrackerbackend/CoronaTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackforfun.coronatrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
