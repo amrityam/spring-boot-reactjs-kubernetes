77bd2d4d66e017907e2da472f3a5fb6dd7fa9a9c
Amrityam_Rout <amrityamlive@gmail.com>
2020-12-29 16:26:48 +0530

    update icon position in Navbar

--- corona-tracker-frontend/src/components/Navigation.js
+++ corona-tracker-frontend/src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Navbar,Nav } from 'react-bootstrap';
-import {LinkContainer} from 'react-router-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import spring_boot_logo from '../images/spring-boot-logo.svg';
 import react_logo from '../images/react-logo.svg';
 import docker_logo from '../images/docker-logo.svg';
@@ -13,84 +13,77 @@ class Navigation extends React.Component {
 
   render() {
     return (
-      
+
       <div>
-        <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">
-    <div>
-    <img
-        alt="covid19"
-        src={covid19_virus}
-        width="90"
-        height="30"
-        className="d-inline-block align-top"
-      />
-      COVID-19 Live Tracker
-    <img
-        alt="spring-boot"
-        src={spring_boot_logo}
-        width="90"
-        height="30"
-        className="d-inline-block align-top"
-      />
-      <img
-        alt="reactjs"
-        src={react_logo}
-        width="50"
-        height="30"
-        className="d-inline-block align-top"
-      />
-      <img
-        alt="docker"
-        src={docker_logo}
-        width="75"
-        height="40"
-        className="d-inline-block align-top"
-      />
-      <img
-        alt="kubernetes"
-        src={k8_logo}
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-      />{' '}
 
-    </div>
-     
-      
-    </Navbar.Brand>
-  </Navbar>
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 
-          
-          
+          <Navbar.Brand><img
+            alt="covid19"
+            src={covid19_virus}
+            width="90"
+            height="30"
+            className="d-inline-block align-top"
+          /></Navbar.Brand>
+
+          <Navbar.Brand>COVID-19 Live Tracker</Navbar.Brand>
+
+
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          
+
           <Navbar.Collapse id="responsive-navbar-nav">
-             <Nav className="mr-auto">
+            <Nav className="mr-auto">
               <LinkContainer to="/dashboard">
-                  <Nav.Link>Dashboard</Nav.Link>
+                <Nav.Link>Dashboard</Nav.Link>
               </LinkContainer>
             </Nav>
             <Nav className="mr-auto">
               <LinkContainer to="/service">
-                  <Nav.Link>Service</Nav.Link>
+                <Nav.Link>Service</Nav.Link>
               </LinkContainer>
             </Nav>
             <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
+            <Navbar.Brand><img
+              alt="spring-boot"
+              src={spring_boot_logo}
+              width="90"
+              height="30"
+              className="d-inline-block align-top"
+            />
+              <img
+                alt="reactjs"
+                src={react_logo}
+                width="50"
+                height="30"
+                className="d-inline-block align-top"
+              />
+              <img
+                alt="docker"
+                src={docker_logo}
+                width="75"
+                height="40"
+                className="d-inline-block align-top"
+              />
+              <img
+                alt="kubernetes"
+                src={k8_logo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              /></Navbar.Brand>
             <Nav className="mr-auto">
               <LinkContainer to="/about">
                 <Nav.Link>About</Nav.Link>
               </LinkContainer>
             </Nav>
-            
+
           </Navbar.Collapse>
         </Navbar>
       </div>
-      
+
     );
   }
-  
+
 }
 
 export default Navigation;
